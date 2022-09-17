f98008110963af24dbb553e80099c0f334804d83
Samuel Wiseman <dontpeak@github.com>
2022-09-17 13:31:19 -0500

    last

--- app/src/main/java/jrogueclone/entity/Player.java
+++ app/src/main/java/jrogueclone/entity/Player.java
@@ -16,8 +16,8 @@ public class Player extends Entity {
     public void handleEntitySpawn() {
 
         // Give the player a weapon with a 70% chance to enflict 34 damage
-        this.m_AvailableWeapons[0] = new Weapon("Damaged Wooden Sword",
-                34, 70);
+        this.m_AvailableWeapons.add( new Weapon("Damaged Wooden Sword",
+                34, 70));
         
         this.getHealthController().setHealth(100);
         this.m_TilesPerSecond = 1;
