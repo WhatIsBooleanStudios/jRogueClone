5b7ec24fdaeb4ad63990215ff34e179477e17fd7
Samuel Wiseman <dontpeak@github.com>
2022-09-17 13:35:06 -0500

    fix nullptr exception

--- app/src/main/java/jrogueclone/entity/Bat.java
+++ app/src/main/java/jrogueclone/entity/Bat.java
@@ -11,8 +11,8 @@ public class Bat extends Entity {
     @Override
     public void handleEntitySpawn() {
         // Give the player a weapon with a 50% chance to enflict 12 damage
-        this.m_AvailableWeapons[0] = new Weapon("Fangs",
-                12, 50);
+        this.m_AvailableWeapons.add(new Weapon("Fangs",
+                12, 50));
 
         this.getHealthController().setHealth(20);
         this.m_TilesPerSecond = 1;
